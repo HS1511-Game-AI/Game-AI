c737a60c56e12037fd178777af5b9e83bbcf9fd1
Djimon Jayasundera <woofless77@gmail.com>
2018-05-04 12:42:29 +0800

    Made AI start a spinoff when enough resources

--- mrPass.c
+++ mrPass.c
@@ -14,10 +14,18 @@
 #include "mechanicalTurk.h"
  
 action decideAction (Game g) {
- 
-   
-   action nextAction;
-   nextAction.actionCode = PASS;
-   
-   return nextAction;
+    MJStudents = getStudents(g, getWhoseTurn(g), STUDENT_MJ);
+    MTVStudents = getStudents(g, getWhoseTurn(g), STUDENT_MTV);
+    MMONEYStudents = getStudents(g, getWhoseTurn(g), STUDENT_MMONEY);
+    
+    action nextAction;
+    
+    if (MJStudents > 0 && MTVStudents > 0 && MMONEYStudents > 0) {
+        //Start spinoff
+        nextAction.actionCode = START_SPINOFF;
+    } else {
+        // Pass
+        nextAction.actionCode = PASS;
+    }
+    return nextAction;
 }
