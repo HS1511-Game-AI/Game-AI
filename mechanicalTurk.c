281cf63c841ee7aa49ad762c9d434839fff26c1f
Alistair <1011776@student.ccgs.wa.edu.au>
2018-05-12 23:56:52 +0800

    Copy mrPass into mechanical turk and work on ARC stuff

--- mechanicalTurk.c
+++ mechanicalTurk.c
@@ -4,3 +4,38 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "mechanicalTurk.h"
+#include "Game.h"
+
+
+// Just an idea but we build an arc 1st turn using one of these
+#define UNI_A_ARC_PATH_1 ""
+#define UNI_A_ARC_PATH_1 "br"
+#define UNI_A_ARC_PATH_1 "lrlrlrlrll"
+#define UNI_A_ARC_PATH_1 "lrlrlrlrlll"
+
+#define UNI_B_ARC_PATH_1 ""
+#define UNI_B_ARC_PATH_1 ""
+#define UNI_B_ARC_PATH_1 ""
+#define UNI_B_ARC_PATH_1 ""
+
+#define UNI_C_ARC_PATH_1 ""
+#define UNI_C_ARC_PATH_1 ""
+#define UNI_C_ARC_PATH_1 ""
+#define UNI_C_ARC_PATH_1 ""
+
+action decideAction (Game g) {
+    int MJStudents = getStudents(g, getWhoseTurn(g), STUDENT_MJ);
+    int MTVStudents = getStudents(g, getWhoseTurn(g), STUDENT_MTV);
+    int MMONEYStudents = getStudents(g, getWhoseTurn(g), STUDENT_MMONEY);
+    
+    action nextAction;
+    
+    if (MJStudents > 0 && MTVStudents > 0 && MMONEYStudents > 0) {
+        //Start spinoff
+        nextAction.actionCode = START_SPINOFF;
+    } else {
+        // Pass
+        nextAction.actionCode = PASS;
+    }
+    return nextAction;
+}
